75707f1b165861bc8a11692c65477400b13ba548
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-08-08 15:47:12 -0500

    .

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,5 @@
-// middleware.ts
-import createMiddleware from 'next-intl/middleware';
-
-export default createMiddleware({
+export const routing = {
   locales: ['en', 'es'],
   defaultLocale: 'es',
   localePrefix: 'as-needed'
-});
-
-export const config = {
-  matcher: [
-    // Ejecuta i18n en todo MENOS estas rutas/archivos
-    '/((?!api|_next|static|odoo|longpolling|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)'
-  ]
 };
